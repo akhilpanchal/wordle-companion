31170cfa5e1b66993a9b6d890e4f49f275552981
Akhil Panchal <akhilpanchal@gmail.com>
2022-02-04 18:24:35 -0500

    feat: Add footer with contributors

--- src/App.js
+++ src/App.js
@@ -7,14 +7,20 @@ import 'react-toastify/dist/ReactToastify.css';
 
 export default function App() {
   return (
-    <div className="App">
-      <h1>WORDLE GENIE</h1>
-      <h4>1. Enter the word suggested here in the game.</h4>
-      <h4>2. Give the hints by tapping over the letters to change the hint type.</h4>
-      <h4>3. Click Go! to get the next word suggestion.</h4>
-      <WordleContextProvider>
-        <Board />
-      </WordleContextProvider>
-    </div>
+    <>
+      <div className="App">
+        <h1>WORDLE GENIE</h1>
+        <h4>1. Enter the word suggested here in the game.</h4>
+        <h4>2. Give the hints by tapping over the letters to change the hint type.</h4>
+        <h4>3. Click Go! to get the next word suggestion.</h4>
+        <WordleContextProvider>
+          <Board />
+        </WordleContextProvider>
+      </div>
+      <footer className="footer">
+        <div>Built with ❤️ by&nbsp;<a href="https://github.com/akhilpanchal">@akhilpanchal</a>&nbsp;and <a href="https://github.com/infinitymittal">@infinitymittal</a>.&nbsp;</div>
+        <br />
+      </footer>
+    </>
   );
 }
